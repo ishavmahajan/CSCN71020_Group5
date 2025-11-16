6599991ed952abbb914c03f8064eec08ed41b47d
Ishav Mahajan <ishavm2800@gmail.com>
2025-11-16 07:43:11 -0500

    angleFinder function made for the triangle, intergrated it to the main.c file, also made a small validationc change to the triangle solver function

--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -2,3 +2,4 @@
 void printWelcome();
 int printShapeMenu();
 int* getTriangleSides(int* triangleSides);
+void anglefinder(int side1, int side2, int side3, double* angleA, double* angleB, double* angleC);
--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -5,23 +5,28 @@
 
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
-	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
-		result = "Invalid input: sides must be positive numbers.";
-	}
-	else if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
-		result = "Not a triangle";
-	}
-	else if (side1 == side2 && side1 == side3) {
-		result = "Equilateral triangle";
-	}
-	else if ((side1 == side2 && side1 != side3) || 
-		(side1 == side3 && side1 != side2))
-	{
-		result = "Isosceles triangle";
-	}
-	else {
-		result = "Scalene triangle";
-	}
+	if (side1 + side2 >= side3 &&
+		side1 + side3 >= side2 &&
+		side2 + side3 >= side1) {
+		if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+			result = "Invalid input: sides must be positive numbers.";
+		}
+		else if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+			result = "Not a triangle";
+		}
+		else if (side1 == side2 && side1 == side3) {
+			result = "Equilateral triangle";
+		}
+		else if ((side1 == side2 && side1 != side3) ||
+			(side1 == side3 && side1 != side2))
+		{
+			result = "Isosceles triangle";
+		}
+		else {
+			result = "Scalene triangle";
+		}
 
-	return result;
+		return result;
+	}
+	else { return "The sides cannot form a triangle"; }
 }
