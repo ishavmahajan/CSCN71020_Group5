d0e286549a9431660611a325a7a816fa94e2fc15
Ishav Mahajan <ishavm2800@gmail.com>
2025-11-25 11:29:02 -0500

    Corrected the rectangle functionality

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -1,20 +1,7 @@
 #include <math.h>
 
-void rectangleSideLength(int xpoints[], int ypoints[]) {
-	// Calculate the lengths of the sides
-	double side1 = sqrt(pow(xpoints[1] - xpoints[0], 2) + pow(ypoints[1] - ypoints[0], 2));
-	double side2 = sqrt(pow(xpoints[2] - xpoints[1], 2) + pow(ypoints[2] - ypoints[1], 2));
-	double side3 = sqrt(pow(xpoints[3] - xpoints[2], 2) + pow(ypoints[3] - ypoints[2], 2));
-	double side4 = sqrt(pow(xpoints[0] - xpoints[3], 2) + pow(ypoints[0] - ypoints[3], 2));
-
-}
-
-void rectangleSidePrint(int side1, int side2, int side3, int side4) {
-	printf_s("Side lengths:\n");
-	printf_s("Side 1: %.2lf\n", side1);
-	printf_s("Side 2: %.2lf\n", side2);
-	printf_s("Side 3: %.2lf\n", side3);
-	printf_s("Side 4: %.2lf\n", side4);
+double RectanglesideLength(int x1, int y1, int x2, int y2) {
+	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
 double rectangleArea(double side1, double side2) {
@@ -24,3 +11,26 @@ double rectangleArea(double side1, double side2) {
 double rectanglePerimeter(double side1, double side2, double side3, double side4) {
 	return side1 + side2 + side3 + side4;
 }
+
+void printSideLengths(double side1, double side2, double side3, double side4) {
+	printf("Side lengths are: \n");
+	printf("Side 1: %.2lf\n", side1);
+	printf("Side 2: %.2lf\n", side2);
+	printf("Side 3: %.2lf\n", side3);
+	printf("Side 4: %.2lf\n", side4);
+}
+
+double RectangleDiagonalLength(int x1, int y1, int x2, int y2) {
+	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+char* analyzeRectangle(double side1, double side2, double side3, double side4, double diagonal1, double diagonal2) {
+		
+	if (side1 == side3 && side2 == side4 && diagonal1 == diagonal2 && (side1 * side1 + side2 * side2 == diagonal1 * diagonal1, 0.001)) {
+		return "Valid rectangle"; //also added pythragorean theorem check
+	}
+	else {
+		return "Invalid rectangle";
+	}
+}
+
--- PolygonChecker/rectangleSolver.h
+++ PolygonChecker/rectangleSolver.h
@@ -1,1 +1,7 @@
 #pragma once
+double RectanglesideLength(int x1, int y1, int x2, int y2);
+void printSideLengths(double side1, double side2, double side3, double side4);
+double RectangleDiagonalLength(int x1, int y1, int x2, int y2);
+char* analyzeRectangle(double side1, double side2, double side3, double side4, double diagonal1, double diagonal2);
+double rectangleArea(double side1, double side2);
+double rectanglePerimeter(double side1, double side2, double side3, double side4);
